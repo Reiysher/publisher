77c59a6128152892124459c85ce3dde9256c8483
Maksim Kushniruk <reiysher25@gmail.com>
2022-06-21 19:53:28 +0300

    Move config file

--- Publisher/Config/ConfigurationHelper.cs
+++ Publisher/Config/ConfigurationHelper.cs
@@ -2,13 +2,11 @@
 
 internal class ConfigurationHelper
 {
-    private static IConfiguration _configuration;
-
     public static PublisherOptions GetConfiguration()
     {
         var builder = new ConfigurationBuilder()
             .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("config.json", optional: false);
+                .AddJsonFile("Config/config.json", optional: false);
 
         return builder.Build().GetSection(nameof(PublisherOptions)).Get<PublisherOptions>();
     }
